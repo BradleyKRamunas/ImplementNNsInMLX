258604c6344e682ea85c6437e94d048b0bc333ba
BradleyKRamunas <bradleyr@cs.stanford.edu>
2025-08-18 22:22:00 -0700

    added simple eval at end and example output

--- imdb_sent_classification_nn.py
+++ imdb_sent_classification_nn.py
@@ -116,6 +116,15 @@ class SentimentClassifier(nn.Module):
             input = layer(input)
             input = nn.relu(input)
         return self.layers[-1](input)
+    
+    def eval(self, x: mx.array):
+        embedded = self.embedding_layer(x)
+        input = mx.flatten(embedded, start_axis=1)
+        for layer in self.layers[:-1]:
+            input = layer(input)
+            input = nn.relu(input)
+        logits = self.layers[-1](input)
+        return mx.argmax(logits, axis=1)
 
 
 def loss_fn(model: nn.Module, input: mx.array, target: mx.array):
@@ -197,7 +206,78 @@ def main():
         losses.append(current_loss)
     print("!!!!!!!!!! Training Complete !!!!!!!!!!")
     print("losses: ", losses)
+    train_predictions = model.eval(training_inputs)
+    train_labels_argmax = mx.argmax(training_labels, axis=1)
+    train_count_correct = mx.sum(train_predictions == train_labels_argmax)
+    train_count_total = train_labels_argmax.shape[0]
+    
+    print(f'\nTrain Evaluation result: Got {train_count_correct} / {train_count_total} ({100 * train_count_correct / train_count_total}%) correct')
+    
+    test_predictions = model.eval(test_inputs)
+    test_labels_argmax = mx.argmax(test_labels, axis=1)
+    test_count_correct = mx.sum(test_predictions == test_labels_argmax)
+    test_count_total = test_labels_argmax.shape[0]
+    
+    print(f'\nTest Evaluation result: Got {test_count_correct} / {test_count_total} ({100 * test_count_correct / test_count_total}%) correct')
 
 
 if __name__ == "__main__":
     main()
+
+"""
+Example output:
+
+training inputs size:  122847
+training inputs shape:  (122847, 64)
+training labels shape:  (122847, 2)
+test inputs shape:  (30001, 64)
+test labels shape:  (30001, 2)
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 0 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.5929447412490845
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 1 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.5483018755912781
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 2 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.5887638926506042
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 3 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.49543753266334534
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 4 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.5944880247116089
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 5 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.2962203025817871
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 6 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.4095418453216553
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 7 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.284853994846344
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 8 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.30930444598197937
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 9 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.11571268737316132
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 10 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.10726296156644821
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 11 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.17406384646892548
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 12 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.040653545409440994
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 13 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.08426908403635025
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 14 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.05074501037597656
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 15 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.13719087839126587
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 16 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.10115986317396164
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 17 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.05432279035449028
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 18 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.20951376855373383
+*+*+*+*+*+*+*+*+*+*+ EPOCH STATS FOR EPOCH 19 *+*+*+*+*+*+*+*+*+*+
+ loss:  0.12259147316217422
+!!!!!!!!!! Training Complete !!!!!!!!!!
+losses:  [0.5929447412490845, 0.5483018755912781, 0.5887638926506042, 0.49543753266334534, 0.5944880247116089, 0.2962203025817871, 0.4095418453216553, 0.284853994846344, 0.30930444598197937, 0.11571268737316132, 0.10726296156644821, 0.17406384646892548, 0.040653545409440994, 0.08426908403635025, 0.05074501037597656, 0.13719087839126587, 0.10115986317396164, 0.05432279035449028, 0.20951376855373383, 0.12259147316217422]
+
+Train Evaluation result: Got 119432 / 122847 (97.2201156616211%) correct
+
+Test Evaluation result: Got 24300 / 30001 (80.99729919433594%) correct
+
+So... quite a bit of overfitting! But not super bad
+"""
